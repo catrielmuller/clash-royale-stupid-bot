e6ca767296cca835b83cfbde2bc7f6684d3a9313
Catriel Müller <catrielmuller@gmail.com>
2017-01-03 11:33:42 -0300

    Progress Bar

--- bot.sh
+++ bot.sh
@@ -2,19 +2,30 @@
 
 clear
 echo "Clash Royale Stupid Bot"
+echo "-----------------------"
+echo ""
 
 for (( i=1; i<=200; i++ ))
 do
-  echo "Start Match $i"
+  echo "- Safe Prepare match"
+  adb shell input tap 568 1796
+  sleep 2
+  adb shell input tap 568 1796
+  sleep 2
+  echo "- Start Match $i"
   adb shell input tap 608 1167
   sleep 5
   adb shell input tap 544 1204
-  echo "Waiting to start the battle"
+  echo "- Waiting to start the battle"
   sleep 10
-  echo "Start Battle"
+  echo "- Start Battle"
+
+  BAR='###############'
 
   for (( j=1; j<=15; j++ ))
   do
+    echo -ne "\r[$j/15] ${BAR:0:$j}"
+
     adb shell input tap 336 1829
     sleep 1
     adb shell input tap 581 260
@@ -36,9 +47,11 @@ do
     sleep 2
   done
 
-  echo "Waiting to end battle screen"
+  echo ""
+  echo "- Waiting to end battle screen"
   sleep 5
   adb shell input tap 608 1665
-  echo "------------------------------"
-  sleep 5
+  echo ""
+  echo ""
+  sleep 2
 done
